a5c9dc2898ede95a7a6b1d0203c2f01a6a16282c
Sara Bornstein <sbornste@gmail.com>
2020-04-12 21:31:02 -0800

    VarArgs

---
+++ src/CodingNomads/examples/methods/VarArgsExamples.java
@@ -0,0 +1,22 @@
+package CodingNomads.examples.methods;
+
+public class VarArgsExamples {
+    public static void main(String[] args) {
+
+        int count1 = countArgs("one", "two", "three");
+        int count2 = countArgs("one", "two");
+        int count3 = countArgs("one");
+
+        System.out.println("count1 is " + count1);
+        System.out.println("count2 is " + count2);
+        System.out.println("count3 is " + count3);
+    }
+
+    public static int countArgs(String...args){ //"..." means varArgs or variable length arguments, varargs must be the last parameter listed.
+        int count =0;
+        for(String s : args){
+            count++;
+        }
+        return count;
+    }
+}
